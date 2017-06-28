022c4bfbb8e947e373bcb74701b502c1ce88f946
RodrigoGandra <rodrigo.gandra@hotmail.com>
2017-06-28 11:50:45 -0300

    teste de generalizacao do banco para hepatite adicionado

--- src/get_data.php
+++ src/get_data.php
@@ -73,6 +73,33 @@ while($i<count($pedacos)){
 		}
 		file_put_contents('data.txt', $p, FILE_APPEND);
 	}
+	if($pedacos[$i] == "Hepatite"){
+		//CONEXÃO COM O BANCO
+		$bdcon = pg_connect("host={$_ENV['gs_host']} port={$_ENV['gs_port']} dbname='ocorrencia_hepatite' user={$_ENV['gs_user']} password={$_ENV['gs_password']}");
+		//SELECT QUE REALIZA A CONVERSÃO DE UTM PARA LAT/LONG
+		$result = pg_exec($bdcon, "SELECT ST_X(the_geom), ST_Y(the_geom)  FROM(SELECT ST_Transform
+			(the_geom, 4326) as the_geom from hepatitea) g");
+		
+		//VARIÁVEIS RESPONSÁVEIS PELO CONTROLE DO WHILE
+		$ct = pg_fetch_all($result);
+		$m = count($ct);
+		$i = 0;
+		while($i < $m){
+			//REPETIÇÃO PARA PREENCHER O GEOJSON  VIA FETCH ASSOC
+			while ($row = pg_fetch_assoc($result)) { 
+      		$x =  $row['st_x'];
+   		   	$y =  $row['st_y'];
+   		   	//CONDIÇÃO PARA CONCATENAR O GEOJSON POIS A ÚLTIMA LINHA SE DIFERE DAS DEMAIS
+			if($i<$m-1){
+				$json.= '{ "type": "Feature","geometry": {"type": "Point","coordinates": ['.$x.','.$y.']},"properties": {"prop0": "value0","prop1": 0.0}},';
+			}else{
+				$json.= '{ "type": "Feature","geometry": {"type": "Point","coordinates": ['.$x.','.$y.']},"properties": {"prop0": "value0","prop1": 0.0}}';
+			}
+			//die();
+			$i = $i+1;
+			}
+		}
+	}
 	if($pedacos[$i] == "Dengue"){
 		//CONEXÃO COM O BANCO
 		$bdcon = pg_connect("host={$_ENV['gs_host']} port={$_ENV['gs_port']} dbname={$_ENV['gs_dbname_dengue']} user={$_ENV['gs_user']} password={$_ENV['gs_password']}");
@@ -105,6 +132,3 @@ while($i<count($pedacos)){
 }
 $json.="]}";
 echo $json;
-
-
-?>
