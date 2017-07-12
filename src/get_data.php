2cb28d942c6069fa74b6bc6940e95c563064cd87
RodrigoGandra <rodrigo.gandra@hotmail.com>
2017-07-12 10:25:57 -0300

    todos pontos adicionados

--- src/get_data.php
+++ src/get_data.php
@@ -1,20 +1,20 @@
 <?php
 //VERSÃO UTILIZADA, MOSTRANDO VÁRIAS REQUISISÕES AO MESMO TEMPO
-//var_dump($_POST);
-
 $i = 0;
 $database = $_REQUEST['database'];
-$pedacos = explode(",", $database);
+//$DATABASE = INFORMAÇÕES DA CHECKBOX QUE SERÃO NECESSÁRIAS NA CONSULTA
 $json = "";
 $json.='{"type": "FeatureCollection","features": [';
 
 if(!empty($database)){
-	$database = explode(',', $database);
+	$database = explode(',', $database); 
+	//SEPARA AS INFORMAÇÕES DA CHECKBOX EM UM ARRAY
+	//database = [NOMEBANCO][NOMETABELA]
 	$dbname= $database[0];
 	$tablename = $database[1];
 	//CONEXÃO COM O BANCO
  	$bdcon = pg_connect("host={$_ENV['gs_host']} port={$_ENV['gs_port']} dbname=".$dbname." user={$_ENV['gs_user']} password={$_ENV['gs_password']}");
-	//SELECT QUE REALIZA A CONVERSÃO DE UTM PARA LAT/LONG
+	//SELECT GENÉRICA (APENAS PARA PONTOS) QUE REALIZA A CONVERSÃO DE UTM PARA LAT/LONG
  	$result = pg_exec($bdcon, "SELECT ST_X(the_geom), ST_Y(the_geom) FROM(SELECT ST_Transform
  	(the_geom, 4326) as the_geom from ".$tablename.") g");
 	//VARIÁVEIS RESPONSÁVEIS PELO CONTROLE DO WHILE
