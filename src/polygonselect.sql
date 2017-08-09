437fe7ece70a842ab6d148b3e64288e231b2b260
Erick563 <ericksantosmartins2010@hotmail.com>
2017-08-09 11:06:12 -0300

    testes adicionados multipolygon

--- src/polygonselect.sql
+++ src/polygonselect.sql
@@ -23,3 +23,20 @@ SELECT ST_X(the_geom), ST_Y(the_geom), logradouro FROM(SELECT ST_Transform
 SELECT ST_AsGeoJSON(the_geom) as geojson , logradouro FROM(SELECT ST_Transform
 			(the_geom, 4326) as the_geom, logradouro from armadilhas) g
 
+
+SELECT ST_X(the_geom), ST_Y(the_geom)
+       FROM bairros;
+
+SELECT ST_AsLatLonText(ST_DumpPoints(the_geom)) from bairros
+
+SELECT  ST_Boundary(ST_Dump(the_geom)) from bairros;
+SELECT ST_Boundary() from bairros
+
+SELECT AsText(InteriorRingN(GeometryN(the_geom))) FROM bairros;
+
+https://gisunchained.wordpress.com/2015/11/06/getting-multipolygon-vertexes-using-postgis/
+
+https://gis.stackexchange.com/questions/108527/extract-lat-lon-points-from-polygon
+https://gis.stackexchange.com/questions/65025/how-to-calculate-x-y-z-m-in-postgis
+https://gis.stackexchange.com/questions/42970/how-to-get-coordinates-from-geometry-in-postgis
+
